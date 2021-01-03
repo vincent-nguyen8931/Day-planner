c76cfd07d90fdd45ffd367bdeaedd3fe7502dedb
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-01-03 05:03:10 -0800

    Add function to save user input into local storage

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
 var timeText;
+var rowClicked;
+var rowSelected;
 var timeSwitchCount = 0;
 var time = 1;
 var currentTime = dayjs().hour();
 var possibleTimes = [0, 9, 10, 11, 12, 13, 14, 15, 16, 17];
-// var timeIntToString;
+
+// function to save a value to specified key
+function setJSON(key, value) {
+  window.localStorage.setItem(key, JSON.stringify(value));
+}
+// function to recall a value from specified key
+function getJSON(key) {
+  return JSON.parse(window.localStorage.getItem(key));
+}
 
 // appends current day of the week and date to header section
 $("#headerRow").append("<h2>" + dayjs().format("dddd, MMMM D") + "</h2>");
+
+function checkRow() {
+  switch (rowClicked) {
+    case "saveIcon1":
+      rowClicked = "events-9AM";
+      rowSelected = "#textArea1";
+      break;
+    case "saveIcon2":
+      rowClicked = "events-10AM";
+      rowSelected = "#textArea2";
+      break;
+    case "saveIcon3":
+      rowClicked = "events-11AM";
+      rowSelected = "#textArea3";
+      break;
+    case "saveIcon4":
+      rowClicked = "events-12PM";
+      rowSelected = "#textArea4";
+      break;
+    case "saveIcon5":
+      rowClicked = "events-1PM";
+      rowSelected = "#textArea5";
+      break;
+    case "saveIcon6":
+      rowClicked = "events-2PM";
+      rowSelected = "#textArea6";
+      break;
+    case "saveIcon7":
+      rowClicked = "events-3PM";
+      rowSelected = "#textArea7";
+      break;
+    case "saveIcon8":
+      rowClicked = "events-4PM";
+      rowSelected = "#textArea8";
+      break;
+    case "saveIcon9":
+      rowClicked = "events-5PM";
+      rowSelected = "#textArea9";
+      break;
+  }
+}
 
 function changeTime() {
   switch (timeSwitchCount) {
@@ -95,3 +146,19 @@ function changeRowColor() {
 }
 
 changeRowColor();
+
+for (i = 1; i < 10; i++) {
+  $("#saveIcon" + i).click(saveInput);
+  // $("#saveIcon" + i).click(function () {
+  //   alert("it works");
+  // });
+}
+function saveInput() {
+  rowClicked = this.id;
+  checkRow(rowClicked);
+  console.log($(rowSelected).val())
+  setJSON(rowClicked, $(rowSelected).val());
+
+}
+
+// $("#textArea" + i).textContent
