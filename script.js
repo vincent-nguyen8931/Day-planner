e3f2a09c8a55b2ad55edf9484cf512ece39cd058
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-01-03 20:12:54 -0800

    Add comments to index.html, script.js, and style.css. Also remove unecessary functions.

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function getJSON(key) {
 // appends current day of the week and date to header section
 $("#headerRow").append("<h2>" + dayjs().format("dddd, MMMM D") + "</h2>");
 
+// when function is called, the rowClicked variable is changed in function saveInput at line 149, the switch statement will create the local storage key and store into rowClicked, change rowSelected into textarea ID, and storageLocation into local storage pull of value in that key. 
 function checkRow() {
   switch (rowClicked) {
     case "saveIcon1":
@@ -69,6 +70,7 @@ function checkRow() {
   }
 }
 
+// function changes the text of the time column into the text provided under each case. variable timeSwitchCount is set 0 globally and incremented in the for loop on line 106.
 function changeTime() {
   switch (timeSwitchCount) {
     case 0:
@@ -101,73 +103,58 @@ function changeTime() {
   }
 }
 
-// switch (timeIntToString) {
-//   case 9: return "9AM";
-//   case 10: return "10AM";
-//   case 11: return "11AM";
-//   case 12: return "12PM";
-//   case 13: return "1PM";
-//   case 14: return "2PM";
-//   case 15: return "3PM";
-//   case 16: return "4PM";
-//   case 17: return "5PM";
-// }
-
 // appends rows and columns to the container id planner
 for (i = 1; i < 10; i++) {
+  // calls to the function on line 74 to retrieve the proper text for the time column.
   changeTime();
   // makes a new row and increments time by 1 so that another time slot can be created.
   $("#planner").append("<article class='row' style='margin-left:15%;' id=timeRow" + time++ + "></article>");
   $("#timeRow" + i).append("<article class='col-1' style='text-align:right; padding-top:5px; border:1px solid black;'>" + timeText + "</article>");
   $("#timeRow" + i).append("<article class='col' id='plannerText" + i + "'></article>");
-  // pulls textarea box from bootstrap and appends it to the column plannerText for each row.
+  // pulls textarea box from bootstrap and appends it to the column plannerText for each row. used for taking in user input.
   $("#plannerText" + i).append("<section class='input-group'>");
   $("#plannerText" + i).append("<section class='input-group-append'>");
   $("#plannerText" + i).append("<textarea class='form-control' name='textArea" + i + "' id='textArea" + i + "' aria-label='With textarea'></textarea>");
   $("#plannerText" + i).append("</section>");
   $("#plannerText" + i).append("</section>");
+  // creates a column so that the button image can be appended to it.
   $("#timeRow" + i).append("<article class='col-1' style='border:1px solid black; background:cyan;' id=saveIcon" + i + "></article>");
   $("#saveIcon" + i).append("<i class='far fa-save' style='padding-right: 40%; padding-top: 35%;' id=saveBtn></i>");
+  // increments by one to move switch statement in changeTIme function down to the next case.
   timeSwitchCount++;
 }
 
-function changeRowColor() {
-  // for loop iterates through every row created above
-  for (i = 1; i < 10; i++) {
-    // if statement checks for current time being greater than the array of possible times. When true, the row is given the class "past" to represent a past time.
-    if (currentTime > possibleTimes[i]) {
-      $("#textArea" + i).addClass("past");
-    }
-    // if statement checks for current time being less than the array of possible times. When true, the row is given the class "future" to represent a future time.
-    else if (currentTime < possibleTimes[i]) {
-      $("#textArea" + i).addClass("future");
-    }
-    // if neither of the above are true, then the class "present" will be added to make that row be considered the present time.
-    else {
-      $("#textArea" + i).addClass("present");
-    }
+// for loop iterates through every row created above and sets each time row to varying colors based on past, present, and future time.
+for (i = 1; i < 10; i++) {
+  // if statement checks for current time being greater than the array of possible times. When true, the row is given the class "past" to represent a past time.
+  if (currentTime > possibleTimes[i]) {
+    $("#textArea" + i).addClass("past");
+  }
+  // if statement checks for current time being less than the array of possible times. When true, the row is given the class "future" to represent a future time.
+  else if (currentTime < possibleTimes[i]) {
+    $("#textArea" + i).addClass("future");
+  }
+  // if neither of the above are true, then the class "present" will be added to make that row be considered the present time.
+  else {
+    $("#textArea" + i).addClass("present");
   }
 }
 
-changeRowColor();
-// creates an event listener 
+// creates a click event listener on every button in the column that goes into saveInput function.
 for (i = 1; i < 10; i++) {
   $("#saveIcon" + i).click(saveInput);
 }
 
+// function sets rowClicked to the saveIcon + i from the for loop above. when entering function checkRow, rowClicked will become the local storage name, and rowSelected will become the text area id which is used to pull the user's input into local storage via the setJSON function.
 function saveInput() {
   rowClicked = this.id;
   checkRow();
   setJSON(rowClicked, $(rowSelected).val());
 }
 
-
-function retrieveLocalStorage() {
+// retrieves all information in local storage based on the "events" keys and stores them in their proper row.
   for (i = 1; i < 10; i++) {
-    rowClicked = "saveIcon" +i;
+    rowClicked = "saveIcon" + i;
     checkRow();
     $(rowSelected).val(storageLocation)
   }
-}
-
-retrieveLocalStorage();
